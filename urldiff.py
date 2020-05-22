1e879487abdddb69d9b9e8bb34d37a044080d6f8
Markus Johansson <mjohanss@tibco.com>
2020-05-22 13:25:57 +0200

    urldiff.py uses fileinput and removes closely related urls before printing to stdout

--- urldiff.py
+++ urldiff.py
@@ -1,34 +1,47 @@
 #!/usr/bin/env python3
 
+""" Command line tool to check how similar two urls are two each other
+
+`python3 urldiff.py < wikipedia_urls.txt 2>/dev/null`
+"""
+
 import time
+import functools
+import sys
 import fileinput
-from urllib.parse import urlparse, parse_qs 
-import argparse
+from urllib.parse import urlparse, parse_qs
 import unittest
 
 class TestUrlDistance(unittest.TestCase):
     """ Run with `python3 -m unittest urldiff.py` """
 
-    u1 = urlparse("http://www.google.com/search?q=term#fragment")
-    u2 = urlparse("http://www.google.com/search/path/?q=term#fragment")
-    u4 = urlparse("http://www.google.com/search/path/again?q=term#fragment")
-
     def test_url_distance_equal(self):
-        self.assertEqual(url_distance(self.__class__.u1, self.__class__.u1), 0)
+        u1 = urlparse("http://www.google.com/search?q=term#fragment")
+        u2 = urlparse("http://www.google.com/search/path/?q=term#fragment")
+        self.assertEqual(url_distance(u1, u1), 0)
 
     def test_url_distance_not_equal(self):
-        self.assertNotEqual(url_distance(self.__class__.u1, self.__class__.u2), 0)
+        u1 = urlparse("http://www.google.com/search?q=term#fragment")
+        u2 = urlparse("http://www.google.com/search/path/?q=term#fragment")
+        self.assertNotEqual(url_distance(u1, u2), 0)
 
     def test_path_distance_equal(self):
-        self.assertEqual(path_distance(self.__class__.u1.path, self.__class__.u1.path), 0)
+        u1 = urlparse("http://www.google.com/search?q=term#fragment")
+        u2 = urlparse("http://www.google.com/search/path/?q=term#fragment")
+        self.assertEqual(path_distance(u1.path, u1.path), 0)
 
     def test_path_distance_not_equal(self):
-        self.assertNotEqual(path_distance(self.__class__.u1.path, self.__class__.u2.path), 0)
+        u1 = urlparse("http://www.google.com/search?q=term#fragment")
+        u2 = urlparse("http://www.google.com/search/path/?q=term#fragment")
+        self.assertNotEqual(path_distance(u1.path, u2.path), 0)
 
     def test_url_distance_less(self):
-        d12 = path_distance(self.__class__.u1.path, self.__class__.u2.path)
-        d13 = path_distance(self.__class__.u1.path, self.__class__.u3.path)
-        self.assertLess(d12, d13)
+        u1 = urlparse("http://www.google.com/search?q=term#fragment")
+        u2 = urlparse("http://www.google.com/search/path/?q=term#fragment")
+        u3 = urlparse("http://www.google.com/search/path/again?q=term#fragment")
+        distance_12 = path_distance(u1.path, u2.path)
+        distance_13 = path_distance(u1.path, u3.path)
+        self.assertLess(distance_12, distance_13)
 
 def iter_distance(i1, i2):
     diff = map(lambda t: t[0]!=t[1], zip(i1, i2))
@@ -55,7 +68,7 @@ def ext_distance():
 
 def qs_distance(q1, q2, weight=0.5):
     """ Calculates distance for querystring """
-    qs1 = parse_qs(q1) 
+    qs1 = parse_qs(q1)
     qs2 = parse_qs(q2)
 
     d = 0
@@ -71,17 +84,25 @@ def qs_distance(q1, q2, weight=0.5):
     return d
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser()
-    parser.add_argument("url")
-    args = parser.parse_args()
-    print(args.url)
-    print(type(args.url))
-    args.url
-    cmd_url = urlparse(args.url)
-    print(cmd_url)
-    for line in fileinput.input(files="-"):
+
+    threshold = 4.0
+    unique_urls = []
+
+    for line in fileinput.input():
         raw_url = line.strip()
+
         url = urlparse(line)
-        ud = url_distance(url, cmd_url)
-        pd = path_distance(url.path, cmd_url.path)
-        print("Distance {} (URL: {}, Path: {}) - {}".format(ud + pd, ud, pd, raw_url))
+
+        if not unique_urls:
+            unique_urls.append(url)
+
+        distance_this_url = functools.partial(url_distance, url)
+
+        all_distances = map(distance_this_url, unique_urls)
+        mindistance = min(all_distances)
+
+        if mindistance >= threshold:
+            unique_urls.append(url)
+            print("[Size = {}] Minimum distance {} exceeded threshold {} for URL {}".format(len(unique_urls), mindistance, threshold, raw_url), file=sys.stderr)
+            print(raw_url)
+
