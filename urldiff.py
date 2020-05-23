bdec505d9b1e0bee276afcebc7e83a8e9bf7895f
Markus Johansson <hansmjohansson@gmail.com>
2020-05-23 21:56:41 +0200

    Fix bugs

--- urldiff.py
+++ urldiff.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
-""" Command line tool to check how similar two urls are two each other
+"""
+Command line tool to check how similar two urls are two each other
 
 `python3 urldiff.py < wikipedia_urls.txt 2>/dev/null`
 """
@@ -8,60 +9,102 @@
 import time
 import functools
 import sys
+import os
 import fileinput
-from urllib.parse import urlparse, parse_qs
+from urllib.parse import urlsplit, parse_qs, unwrap
 import unittest
+
+THRESHOLD = 1
 
 def iter_distance(i1, i2):
     diff = map(lambda t: t[0]!=t[1], zip(i1, i2))
-    extra_objects = abs(len(i1) - len(i2))
-    return sum(diff) + extra_objects
+    num_extra_objects = abs(len(i1) - len(i2))
+    return sum(diff) + num_extra_objects
 
-def url_distance(u1, u2, weight=1.0):
-    d = 0
-    d += iter_distance(u1, u2)
-    d += path_distance(u1.path, u2.path)
-    d += qs_distance(u1.query, u2.query)
-    return d * weight
-
-def path_distance(p1, p2, weight=0.5):
-    paths1 = list(filter(None, p1.split('/')))
-    paths2 = list(filter(None, p2.split('/')))
-
-    d = 0
-    d += abs(len(paths1)-len(paths2))
-    d += iter_distance(paths1, paths2)
-    return d * weight
-
-def ext_distance():
+def file_distance(u1, u2):
     raise NotImplementedError()
 
-def qs_distance(q1, q2, weight=0.5):
+def url_distance(u1, u2):
+    # no special handling for these
+    d = iter_distance(
+        (u1.scheme, u1.username, u1.password, u1.hostname, u1.port, u1.fragment),
+        (u2.scheme, u2.username, u2.password, u2.hostname, u2.port, u1.fragment)
+    )
+
+    d += ext_distance(u1, u2)
+    
+    # path
+    d += path_distance(u1, u2)
+
+    # querytring
+    d += qs_distance(u1, u2) * 2
+
+    return d
+
+def path_distance(u1, u2):
+
+    paths1 = list(filter(None, u1.path.split('/')))
+    paths2 = list(filter(None, u2.path.split('/')))
+
+    return iter_distance(paths1, paths2)
+
+def is_url_subset(u1, u2):
+    u1_base = (u1.scheme, u1.hostname, u1.username, u1.password, u1.port) 
+    u2_base = (u2.scheme, u2.hostname, u2.username, u2.password, u2.port)
+
+    if u1_base != u2_base:
+        return False
+
+    if not u2.path.startswith(u1.path):
+        return False
+
+    q1 = parse_qs(u1.query, keep_blank_values=False)
+    q2 = parse_qs(u2.query, keep_blank_values=False)
+    if not all(key in q2 for key in q1):
+        return False
+
+    return True
+
+def ext_distance(u1, u2):
+    _, ext1 = os.path.splitext(u1.path)
+    _, ext2 = os.path.splitext(u2.path)
+
+    def is_extension(ext):
+        return (ext.isalnum() and not ext.isnum())
+
+    d = 0
+
+    if is_extension(ext1) or is_extension(ext2):
+        d = 0 if ext1 == ext2 else 1
+
+    return d
+
+def qs_distance(u1, u2, keys_only=True):
     """ Calculates distance for querystring """
-    qs1 = parse_qs(q1)
-    qs2 = parse_qs(q2)
+    qs1 = parse_qs(u1.query, keep_blank_values=False)
+    qs2 = parse_qs(u2.query, keep_blank_values=False)
 
     d = 0
     if len(qs1) > len(qs2):
-        qs2, qs2 = qs2, qs1
+        qs1, qs2 = qs2, qs1
 
     d += abs(len(qs1) - len(qs2))
 
     for p1, v1 in qs1.items():
         d += 0 if qs2.get(p1) else 1
-        d += 0 if qs2.get(p1) == v1 else 1
+        if not keys_only:
+            d += 0 if qs2.get(p1) == v1 else 1
 
     return d
 
 if __name__ == "__main__":
 
-    threshold = 4.0
     unique_urls = []
 
     for line in fileinput.input():
         raw_url = line.strip()
 
-        url = urlparse(line)
+        url = urlsplit(raw_url)
 
         if not unique_urls:
             unique_urls.append(url)
@@ -71,8 +114,7 @@ if __name__ == "__main__":
         all_distances = map(distance_this_url, unique_urls)
         mindistance = min(all_distances)
 
-        if mindistance >= threshold:
+        if mindistance > THRESHOLD:
             unique_urls.append(url)
-            print("[Size = {}] Minimum distance {} exceeded threshold {} for URL {}".format(len(unique_urls), mindistance, threshold, raw_url), file=sys.stderr)
+            print("[Score {}]".format(mindistance), file=sys.stderr, end=" ")
             print(raw_url)
-
--- urldiff_test.py
+++ urldiff_test.py
@@ -1,56 +1,116 @@
 #!/usr/bin/env python3
 
 import unittest
-from urldiff import url_distance, path_distance, iter_distance, qs_distance
-from urllib.parse import urlparse
+from urldiff import url_distance, path_distance, iter_distance, qs_distance, is_url_subset
+from urllib.parse import urlsplit
 
 class TestUrlDistance(unittest.TestCase):
     """ Run with `python3 -m unittest urldiff.py` """
 
     def test_url_distance_equal(self):
-        u1 = urlparse("http://www.google.com/search?q=term#fragment")
-        u2 = urlparse("http://www.google.com/search/path/?q=term#fragment")
+        u1 = urlsplit("http://www.google.com/search?q=term#fragment")
+        u2 = urlsplit("http://www.google.com/search/path/?q=term#fragment")
         self.assertEqual(url_distance(u1, u1), 0)
         self.assertEqual(url_distance(u2, u2), 0)
 
     def test_url_distance_not_equal(self):
-        u1 = urlparse("http://www.google.com/search?q=term#fragment")
-        u2 = urlparse("http://www.google.com/search/path/?q=term#fragment")
+        u1 = urlsplit("http://www.google.com/search?q=term#fragment")
+        u2 = urlsplit("http://www.google.com/search/path/?q=term#fragment")
         self.assertNotEqual(url_distance(u1, u2), 0)
 
     def test_path_distance_equal(self):
-        u = urlparse("http://www.google.com/search?q=term#fragment")
-        distance_zero = path_distance(u.path, u.path)
+        u = urlsplit("http://www.google.com/search?q=term#fragment")
+        distance_zero = path_distance(u, u)
         self.assertEqual(distance_zero, 0)
 
-        u = urlparse("http://www.google.com/search/path/?q=term#fragment")
-        distance_zero = path_distance(u.path, u.path)
+        u = urlsplit("http://www.google.com/search/path/?q=term#fragment")
+        distance_zero = path_distance(u, u)
         self.assertEqual(distance_zero, 0)
 
     def test_path_distance_not_equal(self):
-        u1 = urlparse("http://www.google.com/search?q=term#fragment")
-        u2 = urlparse("http://www.google.com/search/path/?q=term#fragment")
-        self.assertNotEqual(path_distance(u1.path, u2.path), 0)
+        u1 = urlsplit("http://www.google.com/search?q=term#fragment")
+        u2 = urlsplit("http://www.google.com/search/path/?q=term#fragment")
+        self.assertNotEqual(path_distance(u1, u2), 0)
+
+    def test_path_distance(self):
+        u1 = urlsplit("/")
+        u2 = urlsplit("/one.path")
+        u3 = urlsplit("/one.path/more/")
+
+        self.assertEqual(path_distance(u1,u2), 1)
+        self.assertEqual(path_distance(u2,u3), 1)
 
     def test_url_distance_less(self):
-        u1 = urlparse("http://www.google.com/search?q=term#fragment")
-        u2 = urlparse("http://www.google.com/search/path/?q=term#fragment")
-        u3 = urlparse("http://www.google.com/search/path/again?q=term#fragment")
-        distance_12 = path_distance(u1.path, u2.path)
-        distance_13 = path_distance(u1.path, u3.path)
+        u1 = urlsplit("http://www.google.com/search?q=term#fragment")
+        u2 = urlsplit("http://www.google.com/search/path/?q=term#fragment")
+        u3 = urlsplit("http://www.google.com/search/path/again?q=term#fragment")
+        distance_12 = path_distance(u1, u2)
+        distance_13 = path_distance(u1, u3)
         self.assertLess(distance_12, distance_13)
 
-    def test_qs_distance_equal(self):
-        q1 = urlparse("?a=1&b=b&c=3").query
-        q2 = urlparse("?b=b&c=3&a=1").query
+    def test_qs_distance_less(self):
+        u1 = urlsplit("http://www.google.com/search?a=term")
+        u2 = urlsplit("http://www.google.com/search/path/?a=term&b=term")
+        u3 = urlsplit("http://www.google.com/search/path/?a=term&b=term&c=term")
+        d12 = qs_distance(u1, u2)
+        d13 = qs_distance(u1, u3)
 
-        d1 = qs_distance(q1, q2)
-        d2 = qs_distance(q2, q1)
+        self.assertLess(d12, d13)
+
+    def test_associative(self):
+        u1 = urlsplit("http://www.google.se:80/search//?a=term")
+        u2 = urlsplit("https://www.google.com/search/one/;p=1?a=term&b=term")
+        u3 = urlsplit("http://user:pass@www.google.com/search/path/b/c/d;param=44;param2=12?a=term&b=term&c=ter&a=1m#hash")
+
+        d12, d21 = url_distance(u1, u2), url_distance(u2, u1)
+        self.assertEqual(d12, d21)
+
+        d23, d32 = url_distance(u2, u3), url_distance(u3, u2)
+        self.assertEqual(d23, d32)
+
+        d13, d31 = url_distance(u1, u3), url_distance(u3, u1)
+        self.assertEqual(d13, d31)
+
+    def test_is_url_subset(self):
+        u1 = urlsplit("/file.php?query=value")
+        u2 = urlsplit("http://webmail.com/file.php?query=value")
+        u3 = urlsplit("http://webmail.com/file.php?query=value&another=value")
+        u4 = urlsplit("http://webmail.com/file.php/something/more?query=value&another=value#hash")
+
+        self.assertFalse(is_url_subset(u1, u2))
+        self.assertFalse(is_url_subset(u2, u1))
+        self.assertFalse(is_url_subset(u3, u2))
+
+        self.assertTrue(is_url_subset(u2, u3))
+        self.assertTrue(is_url_subset(u3, u4))
+        self.assertTrue(is_url_subset(u4, u4))
+
+
+    def test_qs_distance_equal(self):
+        u1 = urlsplit("?a=1&b=b&c=3")
+        u2 = urlsplit("?b=b&c=3&a=1")
+
+        d1 = qs_distance(u1, u2)
+        d2 = qs_distance(u2, u1)
 
         self.assertEqual(d1, d2)
 
+    def test_path_distance_equals(self):
+        u1 = urlsplit("/one/two/three")
+        u2 = urlsplit("/one/two/three/four")
+        u3 = urlsplit("/one/one/three/four")
+
+        d = path_distance(u1, u1)
+        self.assertEqual(d, 0)
+
+        d = path_distance(u1, u2)
+        self.assertEqual(d, 1)
+
+        d = path_distance(u1, u3)
+        self.assertEqual(d, 2)
+
     def test_qs_distance_zero(self):
-        q = urlparse("?a=1&b=b&c=3").query
+        q = urlsplit("?a=1&b=b&c=3")
         d = qs_distance(q, q)
         self.assertEqual(d, 0)
 
@@ -73,6 +133,15 @@ class TestUrlDistance(unittest.TestCase):
         a2 = (1,2,3,4)
         self.assertEqual(iter_distance(a1,a2), 1)
 
+    def test_url_difference_big(self):
+        u1 = urlsplit("https://sv.wikipedia.org/w/index.php")
+        u2 = urlsplit("https://sv.wikipedia.org/w/index.php?a=1&b=2&c=3&d=4&e=5")
+
+        d1 = qs_distance(u1, u2)
+        self.assertEqual(d1, 5)
+
+        d2 = url_distance(u1, u2)
+        self.assertEqual(d2, 5)
 
 if __name__ == '__main__':
     unittest.main()
